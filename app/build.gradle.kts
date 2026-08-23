4c5e92046509b249ca1bc0c5537c2bce42a4f373
Drenae <adrien.guilpain@gmail.com>
2026-08-23 06:54:47 +0200

    fix: restore Compose ConstraintLayout dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
 
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.constraintlayout.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.material.icons.core)
