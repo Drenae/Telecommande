0ed43c94b26d4fda9bf57fa95a94b3ee85c31fd2
Drenae <adrien.guilpain@gmail.com>
2026-08-23 08:24:35 +0200

    refactor: remove unused remote connect relay

--- app/src/main/java/com/telecommande/data/repository/remote/RemoteRepository.kt
+++ app/src/main/java/com/telecommande/data/repository/remote/RemoteRepository.kt
@@ -8,11 +8,13 @@ import kotlinx.coroutines.flow.StateFlow
 interface RemoteRepository {
     val tvCoreEvents: Flow<TvCoreEvent>
     val isConnected: StateFlow<Boolean>
-    suspend fun connectToActiveTv(hostAddress: String)
+
     suspend fun disconnectFromTv()
+
     suspend fun sendCommand(
         keyCode: Remotemessage.RemoteKeyCode,
         action: Remotemessage.RemoteDirection = Remotemessage.RemoteDirection.SHORT
     )
+
     fun launchApplication(appLink: String)
 }
