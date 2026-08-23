25fde78ea639c34a9f1da25df9c05ab4bb48437e
Drenae <adrien.guilpain@gmail.com>
2026-08-23 13:04:01 +0200

    style: apply modern teal dark palette

--- app/src/main/java/com/telecommande/ui/theme/Color.kt
+++ app/src/main/java/com/telecommande/ui/theme/Color.kt
@@ -2,46 +2,20 @@ package com.telecommande.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val AppBackgroundColor = Color(0xFF1A1A1A)
-// val ShadowColor = Color.Black // Pas directement utilisé avec Modifier.shadow, mais bonne référence
-
-// Couleurs pour les boutons ronds et les boutons d'application rectangulaires
-val ButtonBackgroundDark = Color(0xFF2A2A2A)
-val ButtonBorderDark = Color(0xFF3A3A3A)
-
-
-// Couleurs pour les boutons rectangulaires avec texte (Retour, Sources)
-val AppButtonTextColor = Color.White // #FFFFFF
-
-val DarkBackground = Color(0xFF1C1C1E)
-val GradientStart = Color(0xFF2C2C2E)
-val GradientEnd = Color(0xFF161616)
-val GradientAppStart = Color(0xFF3A3A3C)
-val GradientAppEnd = Color(0xFF2C2C2E)
-val White = Color.White // Color(0xFFFFFFFF) - déjà défini dans androidx.compose.ui.graphics
-val Black = Color.Black // Color(0xFF000000) - déjà défini
-val Red = Color(0xFFFF0000) // Pourrait être Color.Red
-val Green = Color(0xFF00FF00) // Pourrait être Color.Green
-val PastelGreen = Color(0xFF98FB98)
-
-// Vous pouvez également les regrouper dans un objet si vous préférez
 object AppColors {
-    val darkBackground = Color(0xFF1C1C1E)
-    val gradientStart = Color(0xFF2C2C2E)
-    val gradientEnd = Color(0xFF161616)
-    val gradientAppStart = Color(0xFF3A3A3C)
-    val gradientAppEnd = Color(0xFF2C2C2E)
-    val appWhite = Color.White
-    val appBlack = Color.Black
-    val statusRed = Color.Red
-    val statusGreen = Color.Green
-    val accent = PastelGreen
+    val darkBackground = Color(0xFF0B1114)
+    val surface = Color(0xFF121A1E)
+    val surfaceElevated = Color(0xFF182126)
+    val surfacePressed = Color(0xFF202B30)
+    val border = Color(0xFF27343A)
+
+    val appWhite = Color(0xFFF4F7F8)
+    val textSecondary = Color(0xFF9AA9AF)
+    val appBlack = Color(0xFF071012)
+
+    val accent = Color(0xFF2DD4BF)
+    val accentMuted = Color(0xFF163D39)
+    val statusGreen = Color(0xFF39D98A)
+    val statusAmber = Color(0xFFFFB020)
+    val statusRed = Color(0xFFFF5B5B)
 }
