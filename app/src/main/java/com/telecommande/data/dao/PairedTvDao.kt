12eb61050c8d58f500f3c9456c3f5e4c87b73c5d
Drenae <adrien.guilpain@gmail.com>
2026-08-23 12:35:54 +0200

    refactor: trim unused paired tv DAO queries

--- app/src/main/java/com/telecommande/data/dao/PairedTvDao.kt
+++ app/src/main/java/com/telecommande/data/dao/PairedTvDao.kt
@@ -1,7 +1,6 @@
 package com.telecommande.data.dao
 
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -14,28 +13,12 @@ interface PairedTvDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrUpdate(tvInfo: PairedTvInfo)
 
-    @Delete
-    suspend fun delete(tvInfo: PairedTvInfo)
-
     @Query("DELETE FROM paired_tvs WHERE keystoreAlias = :keystoreAlias")
     suspend fun deleteByKeystoreAlias(keystoreAlias: String)
-
-    @Query("DELETE FROM paired_tvs WHERE ipAddress = :ipAddress")
-    suspend fun deleteByIpAddress(ipAddress: String)
-
 
     @Query("SELECT * FROM paired_tvs WHERE keystoreAlias = :keystoreAlias")
     suspend fun getByKeystoreAlias(keystoreAlias: String): PairedTvInfo?
 
-    @Query("SELECT * FROM paired_tvs WHERE ipAddress = :ipAddress")
-    suspend fun getByIpAddress(ipAddress: String): PairedTvInfo?
-
     @Query("SELECT * FROM paired_tvs")
     fun getAllFlow(): Flow<List<PairedTvInfo>>
-
-    @Query("SELECT * FROM paired_tvs")
-    suspend fun getAll(): List<PairedTvInfo>
-
-    @Query("DELETE FROM paired_tvs")
-    suspend fun clearAll()
 }
