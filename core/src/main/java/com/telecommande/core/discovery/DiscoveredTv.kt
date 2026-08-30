7693e0a14127548f746f5766abbbcc287ab47b10
Drenae <adrien.guilpain@gmail.com>
2026-08-30 16:50:15 +0200

    feat: preserve NSD TXT attributes on discovered TVs

--- core/src/main/java/com/telecommande/core/discovery/DiscoveredTv.kt
+++ core/src/main/java/com/telecommande/core/discovery/DiscoveredTv.kt
@@ -6,12 +6,13 @@ data class DiscoveredTv(
     val serviceName: String,
     val friendlyName: String,
     val hostAddress: InetAddress?,
-    val port: Int
+    val port: Int,
+    val attributes: Map<String, String> = emptyMap()
 ) {
     val ipAddress: String?
         get() = hostAddress?.hostAddress
 
     override fun toString(): String {
-        return "DiscoveredTv(friendlyName='$friendlyName', serviceName='$serviceName', ipAddress='${ipAddress ?: "N/A"}', port=$port)"
+        return "DiscoveredTv(friendlyName='$friendlyName', serviceName='$serviceName', ipAddress='${ipAddress ?: "N/A"}', port=$port, attributes=$attributes)"
     }
 }
