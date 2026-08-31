4f6430d8f55787fc138d42220293976f38b67cd9
Drenae <adrien.guilpain@gmail.com>
2026-08-31 16:14:04 +0200

    feat: tag discovered TVs with protocol type

--- core/src/main/java/com/telecommande/core/discovery/DiscoveredTv.kt
+++ core/src/main/java/com/telecommande/core/discovery/DiscoveredTv.kt
@@ -1,5 +1,6 @@
 package com.telecommande.core.discovery
 
+import com.telecommande.core.protocol.TvProtocolType
 import java.net.InetAddress
 
 data class DiscoveredTv(
@@ -7,12 +8,13 @@ data class DiscoveredTv(
     val friendlyName: String,
     val hostAddress: InetAddress?,
     val port: Int,
-    val attributes: Map<String, String> = emptyMap()
+    val attributes: Map<String, String> = emptyMap(),
+    val protocolType: TvProtocolType = TvProtocolType.ANDROID_TV_REMOTE_V2
 ) {
     val ipAddress: String?
         get() = hostAddress?.hostAddress
 
     override fun toString(): String {
-        return "DiscoveredTv(friendlyName='$friendlyName', serviceName='$serviceName', ipAddress='${ipAddress ?: "N/A"}', port=$port, attributes=$attributes)"
+        return "DiscoveredTv(friendlyName='$friendlyName', serviceName='$serviceName', ipAddress='${ipAddress ?: "N/A"}', port=$port, protocolType=$protocolType, attributes=$attributes)"
     }
 }
