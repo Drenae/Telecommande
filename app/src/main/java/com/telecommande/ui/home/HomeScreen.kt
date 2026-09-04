542e2999caec0c565bfbc676e947a78e3cfd3098
Drenae <adrien.guilpain@gmail.com>
2026-09-04 19:12:11 +0200

    Adjust remote background vertical position

--- app/src/main/java/com/telecommande/ui/home/HomeScreen.kt
+++ app/src/main/java/com/telecommande/ui/home/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -69,7 +70,8 @@ fun HomeScreen(
                 painter = painterResource(R.drawable.remote_background),
                 contentDescription = null,
                 modifier = Modifier.fillMaxSize(),
-                contentScale = ContentScale.Crop
+                contentScale = ContentScale.Crop,
+                alignment = Alignment(0f, -0.82f)
             )
 
             Column(
