4de44c0ace3ac57dd38eab7ef9fb7587688697d6
Drenae <adrien.guilpain@gmail.com>
2026-09-04 18:49:27 +0200

    Centralize remote UI colors

--- app/src/main/java/com/telecommande/ui/theme/Color.kt
+++ app/src/main/java/com/telecommande/ui/theme/Color.kt
@@ -12,10 +12,41 @@ object AppColors {
     val appWhite = Color(0xFFF4F7F8)
     val textSecondary = Color(0xFF9AA9AF)
     val appBlack = Color(0xFF071012)
+    val transparent = Color.Transparent
 
     val accent = Color(0xFF2DD4BF)
     val accentMuted = Color(0xFF163D39)
     val statusGreen = Color(0xFF39D98A)
     val statusAmber = Color(0xFFFFB020)
     val statusRed = Color(0xFFFF5B5B)
+    val statusGray = Color(0xFF808080)
+
+    // Télécommande - surfaces et accents
+    val remoteCyan = Color(0xFF19D7FF)
+    val remoteDeep = Color(0xFF070B10)
+    val remoteRim = Color(0xFF263540)
+    val remoteButtonRim = Color(0xFF33434F)
+    val remoteRaisedTop = Color(0xFF25333E)
+    val remoteRaisedMid = Color(0xFF151E26)
+    val remoteRaisedBottom = Color(0xFF090D12)
+    val remoteConnected = Color(0xFF40E081)
+    val remoteConnectedBorder = Color(0xFF208F5B)
+    val remoteTextMuted = Color(0xFF9AA7B1)
+
+    // D-pad : contraste plus doux pour un rendu bombé plutôt qu'enfoncé
+    val dpadCenter = Color(0xFF24323B)
+    val dpadMid = Color(0xFF18242B)
+    val dpadEdge = Color(0xFF10171C)
+
+    // Volume
+    val volumePanelStart = Color(0xFF0D141B)
+    val volumePanelEnd = Color(0xFF111C24)
+    val volumeTrackInactive = Color(0xFF24323D)
+    val volumeThumbBorder = Color(0xFF6CC9DA)
+
+    // Applications
+    val netflix = Color(0xFFE50914)
+    val youtube = Color(0xFFFF0033)
+    val plex = Color(0xFFE5A900)
+    val crunchyroll = Color(0xFFFF7A00)
 }
