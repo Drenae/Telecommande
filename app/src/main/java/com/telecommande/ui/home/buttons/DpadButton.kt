73f58658bc18b0b369542ce944a53aa26ef8386f
Drenae <adrien.guilpain@gmail.com>
2026-09-05 12:48:29 +0200

    Fix DpadButton layout import

--- app/src/main/java/com/telecommande/ui/home/buttons/DpadButton.kt
+++ app/src/main/java/com/telecommande/ui/home/buttons/DpadButton.kt
@@ -1,5 +1,6 @@
 package com.telecommande.ui.home.buttons
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
