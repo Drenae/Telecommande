dc39e6655027514c27c1deff1934a066f73c72e1
Drenae <adrien.guilpain@gmail.com>
2026-09-05 05:08:04 +0200

    Move outer shadow modifier into existing util package

---
+++ app/src/main/java/com/telecommande/util/ShadowModifiers.kt
@@ -0,0 +1,37 @@
+package com.telecommande.util
+
+import android.graphics.Paint
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+fun Modifier.outerShadow(
+    color: Color = Color.Black,
+    alpha: Float = 0.85f,
+    blurRadius: Dp = 6.dp,
+    offsetY: Dp = 3.dp
+): Modifier = this.then(
+    Modifier.drawBehind {
+        val shadowColor = color.copy(alpha = alpha).toArgb()
+        val paint = Paint().apply {
+            this.color = shadowColor
+            setShadowLayer(
+                blurRadius.toPx(),
+                0f,
+                offsetY.toPx(),
+                shadowColor
+            )
+        }
+
+        drawContext.canvas.nativeCanvas.drawCircle(
+            center.x,
+            center.y,
+            size.minDimension / 2f,
+            paint
+        )
+    }
+)
