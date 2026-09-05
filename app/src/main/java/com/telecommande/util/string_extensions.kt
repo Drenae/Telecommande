61b9b213106f1d599fafa37a60f6a936262b62a5
Drenae <adrien.guilpain@gmail.com>
2026-09-05 12:16:42 +0200

    Add shared pairing PIN normalization

--- app/src/main/java/com/telecommande/util/string_extensions.kt
+++ app/src/main/java/com/telecommande/util/string_extensions.kt
@@ -1,5 +1,17 @@
 package com.telecommande.util
 
-fun String.containsAnyOf(substrings: List<String>, ignoreCase: Boolean = false): Boolean {
-    return substrings.any { this.contains(it, ignoreCase) }
+fun String.containsAnyOf(
+    substrings: List<String>,
+    ignoreCase: Boolean = false
+): Boolean {
+    return substrings.any { substring ->
+        contains(substring, ignoreCase)
+    }
+}
+
+fun String.normalizePairingPin(): String {
+    return uppercase()
+        .filter { character ->
+            character in 'A'..'Z' || character in '0'..'9'
+        }
 }
