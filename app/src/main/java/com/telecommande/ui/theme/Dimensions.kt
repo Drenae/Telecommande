43eb569df53e4c4c6df43ab2180f2d209a30f34d
Drenae <adrien.guilpain@gmail.com>
2026-09-05 07:18:11 +0200

    Centralize project dimensions

--- app/src/main/java/com/telecommande/ui/theme/Dimensions.kt
+++ app/src/main/java/com/telecommande/ui/theme/Dimensions.kt
@@ -1,114 +1,240 @@
 package com.telecommande.ui.theme
 
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-object DefaultButtonColors {
-    val DefaultBackgroundStart = AppColors.surfaceElevated
-    val DefaultBackgroundEnd = AppColors.surface
-    val PressedBackgroundStart = AppColors.surfacePressed
-    val PressedBackgroundEnd = AppColors.surfaceElevated
-    val DefaultBorder = AppColors.border
-
-    val DefaultShadowLight = Color.Transparent
-    val DefaultShadowDark = Color(0x66000000)
-    val PressedShadowLight = Color.Transparent
-    val PressedShadowDark = Color(0x33000000)
-
-    val DefaultBackgroundBrush = Brush.linearGradient(
-        colors = listOf(DefaultBackgroundStart, DefaultBackgroundEnd)
-    )
-    val PressedBackgroundBrush = Brush.linearGradient(
-        colors = listOf(PressedBackgroundStart, PressedBackgroundEnd)
-    )
+object MaterialShapeDimensions {
+    val smallCornerRadius: Dp = 4.dp
+    val mediumCornerRadius: Dp = 8.dp
+    val largeCornerRadius: Dp = 16.dp
 }
 
-object AppSliderColors {
-    val thumbColor = AppColors.appWhite
-    val activeTrackColor = AppColors.accent
-    val inactiveTrackColor = AppColors.surfacePressed
-    val activeTickColor = Color.Transparent
-    val inactiveTickColor = Color.Transparent
+object ShadowModifierDimensions {
+    val defaultBlurRadius: Dp = 6.dp
+    val defaultOffsetY: Dp = 3.dp
+}
+
+object HomeScreenDimensions {
+    val horizontalPadding: Dp = 18.dp
+    val verticalPadding: Dp = 9.dp
+}
+
+object HomeHeaderDimensions {
+    val verticalPadding: Dp = 8.dp
+    val powerButtonSize: Dp = 58.dp
+    val statusDotSize: Dp = 6.dp
+    val statusDotTextSpacing: Dp = 6.dp
+    val statusButtonSize: Dp = 58.dp
+}
+
+object HomeDpadDimensions {
+    val minimumSize: Dp = 240.dp
+    val maximumSize: Dp = 310.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 3.dp
+    val mainBorderWidth: Dp = 1.dp
+    val bottomSpacing: Dp = 8.dp
+    val navigationHorizontalPadding: Dp = 18.dp
+    val navigationToVolumeSpacing: Dp = 10.dp
+    val volumeToMediaSpacing: Dp = 22.dp
+}
+
+object PremiumCircleDimensions {
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 3.dp
+    val mainBorderWidth: Dp = 1.dp
+    val secondaryBorderWidth: Dp = 1.dp
+}
+
+object NavPillDimensions {
+    val cornerRadius: Dp = 26.dp
+    val width: Dp = 92.dp
+    val height: Dp = 52.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 3.dp
+    val mainBorderWidth: Dp = 1.dp
+    val secondaryBorderWidth: Dp = 1.dp
+    val iconSize: Dp = 36.dp
+    val labelTopSpacing: Dp = 4.dp
+}
+
+object VolumeControlDimensions {
+    val contentStartPadding: Dp = 4.dp
+    val contentEndPadding: Dp = 10.dp
+    val labelHorizontalPadding: Dp = 4.dp
+    val labelToSliderSpacing: Dp = 3.dp
+    val sliderCornerRadius: Dp = 19.dp
+    val sliderHeight: Dp = 38.dp
+    val sliderShadowBlurRadius: Dp = 5.dp
+    val sliderShadowOffsetY: Dp = 3.dp
+    val sliderMainBorderWidth: Dp = 1.dp
+    val sliderHorizontalPadding: Dp = 7.dp
+    val thumbSize: Dp = 20.dp
+    val thumbShadowBlurRadius: Dp = 4.dp
+    val thumbShadowOffsetY: Dp = 2.dp
+    val thumbBorderWidth: Dp = 1.5.dp
+    val trackThumbGapSize: Dp = 0.dp
+    val trackInsideCornerSize: Dp = 0.dp
+    val muteButtonSize: Dp = 48.dp
+    val muteButtonTopPadding: Dp = 20.dp
+}
+
+object MediaControlDimensions {
+    val buttonSize: Dp = 62.dp
+}
+
+object HomeFooterDimensions {
+    val verticalPadding: Dp = 10.dp
+    val rowSpacing: Dp = 9.dp
+}
+
+object AppTileDimensions {
+    val cornerRadius: Dp = 17.dp
+    val height: Dp = 66.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 3.dp
+    val mainBorderWidth: Dp = 1.dp
+    val horizontalPadding: Dp = 14.dp
+    val logoSize: Dp = 100.dp
+}
+
+object SettingsDialogDimensions {
+    val renameTechnicalNameSpacing: Dp = 12.dp
+    val renameHelpTextSpacing: Dp = 8.dp
+}
+
+object SettingsScreenDimensions {
+    val topBarTitleStartPadding: Dp = 10.dp
+    val listStartPadding: Dp = 16.dp
+    val listEndPadding: Dp = 16.dp
+    val listTopPadding: Dp = 10.dp
+    val listBottomPadding: Dp = 28.dp
+    val listItemSpacing: Dp = 10.dp
+    val pairedSectionTopSpacing: Dp = 8.dp
+}
+
+object SettingsIconButtonDimensions {
+    val cornerRadius: Dp = 14.dp
+    val startPadding: Dp = 10.dp
+    val size: Dp = 44.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 2.dp
+    val mainBorderWidth: Dp = 1.dp
+    val iconSize: Dp = 22.dp
+}
+
+object SettingsActionButtonDimensions {
+    val cornerRadius: Dp = 18.dp
+    val endPadding: Dp = 12.dp
+    val height: Dp = 42.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 2.dp
+    val mainBorderWidth: Dp = 1.dp
+    val horizontalPadding: Dp = 12.dp
+    val contentSpacing: Dp = 7.dp
+    val iconSize: Dp = 19.dp
+}
+
+object SettingsSectionTitleDimensions {
+    val horizontalPadding: Dp = 2.dp
+    val verticalPadding: Dp = 4.dp
+    val iconContainerSize: Dp = 34.dp
+    val iconContainerCornerRadius: Dp = 10.dp
+    val iconSize: Dp = 19.dp
+    val contentSpacing: Dp = 10.dp
+}
+
+object DiscoveryLoadingDimensions {
+    val cornerRadius: Dp = 18.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 2.dp
+    val mainBorderWidth: Dp = 1.dp
+    val contentPadding: Dp = 18.dp
+    val progressSize: Dp = 26.dp
+    val progressStrokeWidth: Dp = 2.5.dp
+    val progressTextSpacing: Dp = 14.dp
+}
+
+object SettingsStateCardDimensions {
+    val cornerRadius: Dp = 18.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 2.dp
+    val mainBorderWidth: Dp = 1.dp
+    val contentPadding: Dp = 18.dp
+    val iconContainerSize: Dp = 46.dp
+    val iconSize: Dp = 25.dp
+    val iconBottomSpacing: Dp = 10.dp
+    val titleSubtitleSpacing: Dp = 3.dp
+    val actionTopSpacing: Dp = 12.dp
+}
+
+object SettingsPrimaryButtonDimensions {
+    val cornerRadius: Dp = 14.dp
+    val height: Dp = 42.dp
+    val horizontalPadding: Dp = 15.dp
+    val contentSpacing: Dp = 7.dp
+    val iconSize: Dp = 19.dp
+}
+
+object DiscoveredTvItemDimensions {
+    val cornerRadius: Dp = 18.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 2.dp
+    val mainBorderWidth: Dp = 1.dp
+    val contentPadding: Dp = 14.dp
+    val tvIconContainerSize: Dp = 46.dp
+    val tvIconContainerBorderWidth: Dp = 1.dp
+    val tvIconSize: Dp = 24.dp
+    val tvTextSpacing: Dp = 13.dp
+    val actionContainerSize: Dp = 34.dp
+    val actionContainerCornerRadius: Dp = 11.dp
+    val actionIconSize: Dp = 20.dp
+}
+
+object PairedTvItemDimensions {
+    val cornerRadius: Dp = 18.dp
+    val shadowBlurRadius: Dp = 5.dp
+    val shadowOffsetY: Dp = 2.dp
+    val mainBorderWidth: Dp = 1.dp
+    val contentStartPadding: Dp = 14.dp
+    val contentTopPadding: Dp = 13.dp
+    val contentBottomPadding: Dp = 13.dp
+    val contentEndPadding: Dp = 5.dp
+    val tvIconContainerSize: Dp = 46.dp
+    val tvIconContainerBorderWidth: Dp = 1.dp
+    val tvIconSize: Dp = 24.dp
+    val tvTextSpacing: Dp = 13.dp
+    val statusTopPadding: Dp = 5.dp
+    val statusDotSize: Dp = 8.dp
+    val statusTextSpacing: Dp = 6.dp
+    val actionContainerSize: Dp = 34.dp
+    val actionContainerCornerRadius: Dp = 11.dp
+    val renameIconSize: Dp = 18.dp
+    val forgetIconSize: Dp = 19.dp
+}
+
+object PinEntryDialogDimensions {
+    val cornerRadius: Dp = 22.dp
+    val fieldTopSpacing: Dp = 16.dp
+    val overallPadding: Dp = 16.dp
+    val loadingIndicatorSize: Dp = 16.dp
+    val loadingIndicatorStrokeWidth: Dp = 2.dp
+    val loadingTextSpacing: Dp = 8.dp
 }
 
 object HomeBaseButtonSpecs {
-    val DefaultSize: Dp = 74.dp
-    val DefaultIconPadding: Dp = 16.dp
-    val PressedIconReduction: Dp = 2.dp
-    val DefaultBorderWidth: Dp = 1.dp
-    val DefaultElevation: Dp = 5.dp
-    val PressedElevation: Dp = 1.dp
-    val DefaultShape: Shape = CircleShape
+    val defaultSize: Dp = 74.dp
+    val defaultIconPadding: Dp = 16.dp
+    val pressedIconReduction: Dp = 2.dp
+    val defaultBorderWidth: Dp = 1.dp
+    val defaultElevation: Dp = 5.dp
+    val pressedElevation: Dp = 1.dp
 }
 
 object HomeDpadButtonSpecs {
-    val DefaultSize: Dp = 50.dp
+    val defaultSize: Dp = 50.dp
 }
 
 object HomeAppButtonSpecs {
-    val DefaultShape: Shape = RoundedCornerShape(12.dp)
-    val DefaultBackgroundBrush: Brush = DefaultButtonColors.DefaultBackgroundBrush
-    val PressedBackgroundBrush: Brush = DefaultButtonColors.PressedBackgroundBrush
-    val DefaultIconSize: Dp = ComponentDimensions.AppLauncherIconSize
-}
-
-object AppSpecificButtonSizes {
-    val OkButtonSize: Dp = 120.dp
-}
-
-object DpadSectionSpecs {
-    val ContainerSize: Dp = 280.dp
-    val ContainerShape: Shape = CircleShape
-    val BorderWidth: Dp = 1.dp
-    val BorderColor: Color = AppColors.border
-    val BackgroundBrush: Brush = Brush.radialGradient(
-        colors = listOf(AppColors.surfaceElevated, AppColors.surface)
-    )
-    val ArrowButtonMargin: Dp = 1.dp
-}
-
-object ScreenPaddings {
-    val Horizontal: Dp = 18.dp
-}
-
-object ComponentDimensions {
-    val StatusIndicatorSize: Dp = 40.dp
-
-    val NavButtonOffsetNegativeY: Dp = (-32).dp
-
-    val MainControlsInternalPaddingHorizontal: Dp = 8.dp
-    val MainControlsVerticalButtonMarginEnd: Dp = 8.dp
-
-    val DefaultSpacerHeight: Dp = 50.dp
-    val MediumSpacerHeight: Dp = 16.dp
-    val LargeSpacerHeight: Dp = 50.dp
-
-    val FooterPaddingVertical: Dp = 12.dp
-    val AppLauncherGridSpacing: Dp = 6.dp
-    val AppLauncherIconSize: Dp = 32.dp
-    val AppLauncherButtonHeight: Dp = 54.dp
-}
-
-object TvManagementSpecs {
-    val ListItemVerticalPadding: Dp = 5.dp
-    val ListItemHorizontalPadding: Dp = 16.dp
-    val ListItemInternalPadding: Dp = 16.dp
-    val ListItemSpacerWidth: Dp = 14.dp
-    val PairedTvStatusIconSize: Dp = 22.dp
-    val PairedTvForgetButtonSpacer: Dp = 4.dp
-
-    val PinDialogVerticalSpacer: Dp = 16.dp
-    val PinDialogOverallPadding: Dp = 16.dp
-
-    val ScreenSectionTitlePadding: Dp = 16.dp
-    val LazyColumnHorizontalPadding: Dp = 8.dp
-    val DividerVerticalPadding: Dp = 8.dp
-    val EmptyStateIconBottomPadding: Dp = 8.dp
-    val LoadingScreenSpacerHeight: Dp = 8.dp
-    val DisconnectButtonPadding: Dp = 16.dp
+    val defaultIconSize: Dp = 32.dp
 }
